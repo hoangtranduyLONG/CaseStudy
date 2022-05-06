220822ddb54e082fa44cb591027989da8ac3a3f8
hoangtranduyLONG <duylonghindu@gmail.com~>
2022-05-06 16:35:56 +0700

    ghi chú

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,20 +1,37 @@
 package test;
 import java.util.Scanner;
-
         import main.DanhSachSinhVien;
         import main.SinhVien;
-
 public class Test {
+    private static String[] args;
     public static void main(String[] args) {
+        Test.args = args;
         Scanner sc = new Scanner(System.in);
 
         DanhSachSinhVien dssv = new DanhSachSinhVien();
+        String useName="long1999";
+        String passWord="123";
+        boolean checkLogin=true;
+        do{
+            System.out.println("Đăng nhập");
+
+            System.out.println("Nhập tài khoản:");
+            String nhapTaiKhoan= sc.nextLine();
+
+            System.out.println("Nhập Mật Khẩu : ");
+            String nhapMatKhau=sc.nextLine();
+            if(nhapTaiKhoan.equals(useName) && nhapMatKhau.equals(passWord)){
+                checkLogin= false;
+            }else {
+                System.err.println("Nhâp sai tài khoản hoặc mật khẩu . Vui lòng thử lại");
+            }
+        }while (checkLogin);
         int luaChon = 0;
         do {
             System.out.println("MENU ---------- ");
             System.out.println("Vui lòng chọn chức năng: ");
             System.out.println(
-                    "1.	Thêm sinh viên vào danh sách.\n"
+                            "1.	Thêm sinh viên vào danh sách.\n"
                             + "2.	In danh sách sinh viên ra màn hình.\n"
                             + "3.	Kiểm tra danh sách có rỗng hay không.\n"
                             + "4.	Lấy ra số lượng sinh viên trong danh sách.\n"
@@ -26,9 +43,8 @@ public class Test {
                             + "0.   Thoát khỏi chương trình");
             luaChon = sc.nextInt();
             sc.nextLine();
-
+            
             if(luaChon==1) {
-//				1.	Thêm sinh viên vào danh sách.
                 System.out.println("Nhập mã sinh viên: "); String maSinhVien = sc.nextLine();
                 System.out.println("Nhập họ và tên: "); String hoVaTen = sc.nextLine();
                 System.out.println("Nhập năm sinh: "); int namSinh = sc.nextInt();
@@ -37,36 +53,32 @@ public class Test {
                 dssv.themSinhVien(sv);
 
             }else if(luaChon==2) {
-//				2.	In danh sách sinh viên ra màn hình.
-                dssv.inDanhSachSinhVien();
 
+                dssv.inDanhSachSinhVien();
             }else if(luaChon==3) {
-//				3.	Kiểm tra danh sách có rỗng hay không.
                 System.out.println("Danh sách rỗng: "+ dssv.kiemTraDanhSachRong());
             }else if(luaChon==4) {
-//				4.	Lấy ra số lượng sinh viên trong danh sách.
                 System.out.println("Số lượng hiện tại: "+ dssv.laySoLuongSinhVien());
             }else if(luaChon==5) {
-//				5.	Làm rỗng danh sách sinh viên.
+
                 dssv.lamRongDanhSach();
             }else if(luaChon==6) {
-//				6.	Kiểm tra sinh viên có tồn tại trong danh sách hay không, dựa trên mã sinh viên.
+
                 System.out.println("Nhập mã sinh viên: "); String maSinhVien = sc.nextLine();
                 SinhVien sv = new SinhVien(maSinhVien);
                 System.out.println("Kiếm tra sinh viên có trong danh sách: "+dssv.kiemTraTonTai(sv));
             }else if(luaChon==7) {
-//				7.	Xóa một sinh viên ra khỏi danh sách dựa trên mã sinh viên.
                 System.out.println("Nhập mã sinh viên: "); String maSinhVien = sc.nextLine();
                 SinhVien sv = new SinhVien(maSinhVien);
                 System.out.println("Xóa sinh viên trong danh sách: "+dssv.xoaSinhVien(sv));
             }else if(luaChon==8) {
-//				8.	Tìm kiếm tất cả sinh viên dựa trên Tên được nhập từ bàn phím.
+
                 System.out.println("Nhập họ và tên: "); String hoVaTen = sc.nextLine();
                 System.out.println("Kết quả tìm kiếm: ");
                 dssv.timSinhVien(hoVaTen);
 
             }else if(luaChon==9) {
-//				9.	Xuất ra danh sách sinh viên có điểm từ cao đến thấp.
+
                 dssv.sapXepSinhVienGiamDanTheoDiem();
                 dssv.inDanhSachSinhVien();
             }
